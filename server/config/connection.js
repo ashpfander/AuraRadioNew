16371c2afb0ad7abe491fdb5b85b27ed5832da82
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-05-03 21:23:57 -0400

    updated MONGO_URI to MONGODB_URI to match environment variable in render

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/aura-radio', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/aura-radio', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       // useFindAndModify: false,
